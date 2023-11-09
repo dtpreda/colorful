aa4133595b8c2071f0d60c94964b5b3bbe3f029c
David Preda <dtpreda30@gmail.com>
2023-11-09 17:47:39 +0000

    Add support for batch soft_encode

--- src/colour/soft_encode.py
+++ src/colour/soft_encode.py
@@ -1,13 +1,13 @@
 from sklearn.neighbors import NearestNeighbors
-from quantize import get_ab_centroids
+from colour.quantize import get_ab_centroids
 import numpy as np
 
 def soft_encode(ab, centroids=get_ab_centroids(), n=5):
     neigh = NearestNeighbors(n_neighbors=n+1).fit(centroids)
     
     # flatten ab
-    original_shape = ab.shape
-    ab = ab.reshape(ab.shape[0] * ab.shape[1], 2)
+    n, h, w, c = ab.shape
+    ab = ab.reshape(-1, 2)
     distances, indices = neigh.kneighbors(ab)
     distances, indices = distances[:, 1:], indices[:, 1:]
     distances = distances / np.sum(distances, axis=1, keepdims=True)
@@ -15,13 +15,13 @@ def soft_encode(ab, centroids=get_ab_centroids(), n=5):
     encoding = np.zeros((ab.shape[0], centroids.shape[0]), dtype=np.float32)
     encoding[np.arange(ab.shape[0])[:, None], indices] = distances
 
-    encoding = encoding.reshape(original_shape[0], original_shape[1], centroids.shape[0])
+    encoding = encoding.reshape(n, h, w, -1)
 
     return encoding
     
 
 if __name__ == "__main__":
     hull = np.load("data/hull.npy")
-    ab = np.array([[[50, -51], [-52,50]], [[50, -51], [-52,50]]], dtype=np.float32)
+    ab = np.array([[[[50, -51], [-52,50]], [[50, -51], [-52,50]]], [[[50, -51], [-52,50]], [[50, -51], [-52,50]]], [[[50, -51], [-52,50]], [[50, -51], [-52,50]]]], dtype=np.float32)
     encoding = soft_encode(ab, centroids=hull, n=5)
     print(encoding.shape)
