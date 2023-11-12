85eedf92a8b634c2311e72a728805591b4e2c687
David Preda <dtpreda30@gmail.com>
2023-11-12 14:08:33 +0000

    Add args support

--- src/__main__.py
+++ src/__main__.py
@@ -12,13 +12,26 @@ import torch
 from torch.optim import Adam
 from torch import cuda
 
+from argparse import ArgumentParser
 
-BATCH_SIZE = 64
+parser = ArgumentParser()
+parser.add_argument("--batch-size", type=int, default=64)
+parser.add_argument("--epochs", type=int, default=10)
+parser.add_argument("--learning-rate", type=float, default=1e-3)
+parser.add_argument("--num-workers", type=int, default=4)
+parser.add_argument("--gpu", type=int, default=-1)
 
 def import_image(img):
     return torch.FloatTensor(np.transpose(color.rgb2lab(np.array(img)), (2,0,1)))
 
 if __name__ == "__main__":
+    args = parser.parse_args()
+    use_gpu = args.gpu != -1 and torch.cuda.is_available()
+    if use_gpu:
+        print("There are ", torch.cuda.device_count(), " available GPUs!")
+        torch.cuda.set_device(args.gpu)
+        print('Current single GPU: {}'.format(torch.cuda.current_device()))
+    
     hull = np.load("data/hull.npy")
     weights = np.load("data/class_rebalance_weights.npy")
 
@@ -28,23 +41,29 @@ if __name__ == "__main__":
 
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
-                                            shuffle=True, num_workers=4)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
+                                            shuffle=True, num_workers=args.num_workers)
     
     TRAIN_SIZE = len(trainloader)
 
+    device = torch.device(args.gpu if use_gpu else "cpu")
+
     model = Colorizer()
     model.train()
+    model.to(device)
 
-    learning_rate = 1e-3
+    epochs = args.epochs
+    # batch_size = args.batch_size
+    learning_rate = args.learning_rate
+
     optimizer = Adam(model.parameters(), lr=learning_rate)
-    epochs = 10
     train_loss = []
 
     for epoch in range(epochs):
         with tqdm(total=TRAIN_SIZE) as pbar:
             for i, data in enumerate(trainloader):
                 inputs, _ = data
+                inputs = inputs.to(device)
                 l, ab = inputs[:, 0, :, :], inputs[:, 1:, :, :]
                 l = l.unsqueeze(1)
                 prediction = model(l)
