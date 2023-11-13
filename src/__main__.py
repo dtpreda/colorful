38b48011c1f12e94b41d13cc859130b4b8826548
David Preda <dtpreda30@gmail.com>
2023-11-13 10:51:22 +0000

    Update __main__ for test validation

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,6 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
 from src.net.arch import Colorizer
 from src.net.annealed import annealed_mean
 from src.colour.soft_encode import soft_encode
@@ -21,45 +24,59 @@ parser.add_argument("--learning-rate", type=float, default=1e-3)
 parser.add_argument("--num-workers", type=int, default=4)
 parser.add_argument("--gpu", type=int, default=-1)
 
-def import_image(img):
-    return torch.FloatTensor(np.transpose(color.rgb2lab(np.array(img)), (2,0,1)))
-
-if __name__ == "__main__":
-    args = parser.parse_args()
+def set_device(gpu):
     use_gpu = args.gpu != -1 and torch.cuda.is_available()
     if use_gpu:
-        print("There are ", torch.cuda.device_count(), " available GPUs!")
         torch.cuda.set_device(args.gpu)
         print('Current single GPU: {}'.format(torch.cuda.current_device()))
     
-    hull = np.load("data/hull.npy")
-    weights = np.load("data/class_rebalance_weights.npy")
+    device = torch.device(args.gpu if use_gpu else "cpu")
 
+    return device
+
+def get_dataloader(batch_size, num_workers, train=True):
+    def import_image(img):
+        return torch.FloatTensor(np.transpose(color.rgb2lab(np.array(img)), (2,0,1)))
+    
     transform = transforms.Compose([
         transforms.Lambda(import_image),
     ])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    dataset = torchvision.datasets.CIFAR10(root='./data', train=train,
                                             download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
-                                            shuffle=True, num_workers=args.num_workers)
+    
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                            shuffle=True, num_workers=num_workers)
+    
+    return dataloader
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    device = set_device(args.gpu)
+    
+    hull = np.load("data/hull.npy")
+    weights = np.load("data/class_rebalance_weights.npy")
+
+    trainloader = get_dataloader(args.batch_size, args.num_workers, train=True)
+    testloader = get_dataloader(args.batch_size, args.num_workers, train=False)
     
     TRAIN_SIZE = len(trainloader)
-
-    device = torch.device(args.gpu if use_gpu else "cpu")
+    TEST_SIZE = len(testloader)
 
     model = Colorizer()
     model.train()
     model.to(device)
 
     epochs = args.epochs
-    # batch_size = args.batch_size
     learning_rate = args.learning_rate
 
     optimizer = Adam(model.parameters(), lr=learning_rate)
     train_loss = []
+    test_loss = []
 
     for epoch in range(epochs):
+        print("Epoch {}".format(epoch))
         with tqdm(total=TRAIN_SIZE) as pbar:
             for i, data in enumerate(trainloader):
                 inputs, _ = data
@@ -90,8 +107,42 @@ if __name__ == "__main__":
                 pbar.update(1)
             
             pbar.close()
+
+        with tqdm(total=TEST_SIZE) as pbar:
+            print("Testing Epoch {}".format(epoch))
+            for i, data in enumerate(testloader):
+                inputs, _ = data
+                inputs = inputs.to(device)
+                l, ab = inputs[:, 0, :, :], inputs[:, 1:, :, :]
+                l = l.unsqueeze(1)
+                prediction = model(l)
+                prediction = prediction.permute(0, 2, 3, 1)
+                ab = ab.permute(0, 2, 3, 1)
+
+                ground_truth = soft_encode(ab.cpu().numpy(), centroids=hull, n=5)
+                pixelwise_weights = reweight(ground_truth, weights)
+
+                # prediction = torch.FloatTensor(prediction)
+                ground_truth = torch.FloatTensor(ground_truth).to(device)
+                pixelwise_weights = torch.FloatTensor(pixelwise_weights).to(device)
+
+                loss = -torch.sum(pixelwise_weights * torch.sum(ground_truth * torch.log(prediction), dim=-1), dim=(-1, -2))
+                loss = torch.mean(loss)
+
+                test_loss.append(loss.item())
+
+                pbar.update(1)
+            
+            pbar.close()
         
-        print("Epoch {} Loss: {}".format(epoch, np.mean(train_loss)))
+        print("Epoch {} train loss: {}".format(epoch, np.mean(train_loss[-TRAIN_SIZE:])))
+        print("Epoch {} test loss: {}".format(epoch, np.mean(test_loss[-TEST_SIZE:])))
     
     torch.save(model.state_dict(), "model.pth")
+
+    # plot losses
+    matplotlib.use('Qt5Agg')
+    plt.plot(train_loss)
+    plt.plot(test_loss)
+    plt.savefig("loss.png")
     
