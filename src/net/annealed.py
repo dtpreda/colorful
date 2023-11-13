5c82c8fc75fed27e78cb3035ef82f2d64d9d98bf
David Preda <dtpreda30@gmail.com>
2023-11-13 10:35:07 +0000

    Complete annealed_mean module

--- src/net/annealed.py
+++ src/net/annealed.py
@@ -12,9 +12,18 @@ def annealed_mean(z, T=0.38):
 
     return np.sum(f * np.arange(326), axis=-1, keepdims=True)
 
+def mean_to_ab(mean, hull):
+    mean = np.round(mean).astype(np.int32)
+    return hull[mean]
+
+def z_to_y(z):
+    hull = np.load("data/hull.npy")
+    mean = annealed_mean(z)
+    return mean_to_ab(mean, hull)
 
 if __name__ == "__main__":
     distribution = np.load("data/empirical_distribution.npy")
+    hull = np.load("data/hull.npy")
     # get mean of probability distribution
     mean = annealed_mean(np.array([distribution, distribution], dtype=np.float32))
     mean = mean[0]
@@ -26,3 +35,7 @@ if __name__ == "__main__":
     # plot mean as vertical line
     plt.axvline(x=mean, color='r', linestyle='--')
     plt.show()
+
+    # convert mean to ab
+    ab = mean_to_ab(mean, hull)
+    print(ab)
