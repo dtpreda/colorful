1328f7ca9f3575ddd7f69c4b7c51db88c6ffca9e
David Preda <dtpreda30@gmail.com>
2023-11-15 19:03:31 +0000

    Migrate soft_encode to pytorch

--- src/colour/soft_encode.py
+++ src/colour/soft_encode.py
@@ -1,21 +1,18 @@
 from sklearn.neighbors import NearestNeighbors
-from src.colour.quantize import get_ab_centroids
+# from src.colour.quantize import get_ab_centroids
 import numpy as np
 
 import torch
 
-def soft_encode(ab, centroids=get_ab_centroids(), n=5):
-    neigh = NearestNeighbors(n_neighbors=n+1).fit(centroids)
-    
+def soft_encode(ab, centroids, neighbours=5):
     # flatten ab
     n, h, w, c = ab.shape
     ab = ab.reshape(-1, 2)
-    distances, indices = neigh.kneighbors(ab.cpu().numpy())
-    distances, indices = torch.from_numpy(distances.astype(np.float32)).to(ab.device), torch.from_numpy(indices.astype(np.int32)).to(ab.device)
-    distances, indices = distances[:, 1:], indices[:, 1:]
+    distances = torch.cdist(ab, centroids)
+    distances, indices = torch.topk(distances, k=neighbours, dim=-1, largest=False, sorted=True)
     distances = torch.exp(-distances**2 / (2 * 5**2))
     distances = distances / torch.sum(distances, dim=-1, keepdim=True)
-    
+
     encoding = torch.zeros((ab.shape[0], centroids.shape[0]), dtype=torch.float32).to(ab.device)
     encoding[torch.arange(ab.shape[0])[:, None], indices] = distances
 
@@ -23,12 +20,10 @@ def soft_encode(ab, centroids=get_ab_centroids(), n=5):
 
     return encoding
 
-    
-
 if __name__ == "__main__":
     hull = torch.from_numpy(np.load("data/hull.npy"))
     ab = np.array([[[[50, -51], [-52,50]], [[50, -51], [-52,50]]], [[[50, -51], [-52,50]], [[50, -51], [-52,50]]], [[[50, -51], [-52,50]], [[50, -51], [-52,50]]]], dtype=np.float32)
     ab = torch.from_numpy(ab)
-    encoding = soft_encode(ab, centroids=hull, n=5)
+    encoding = soft_encode(ab, hull, neighbours=5)
     print(encoding.shape)
     print(encoding[0, 0, 0, :])
